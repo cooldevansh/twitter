2d46f4ef526cbaaee88e2bc99ddc55fbaa26ceca
cooldevansh <95593706+cooldevansh@users.noreply.github.com>
2022-09-18 11:17:29 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,22 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+/*var firebaseConfig = {
+      apiKey: "AIzaSyCOorZLEVnb4jK18CYFa4SI2HicE9sVIP4",
+      authDomain: "twitter-345a7.firebaseapp.com",
+      databaseURL: "https://twitter-345a7-default-rtdb.firebaseio.com",
+      projectId: "twitter-345a7",
+      storageBucket: "twitter-345a7.appspot.com",
+      messagingSenderId: "1053113413037",
+      appId: "1:1053113413037:web:b469f182b580573d87c524"
+    };
+    
+    // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();/*/
